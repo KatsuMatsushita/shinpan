1322d3abc6b4b7fc883aabec38b9af8a6d522ea2
Hungry Man <katsu.matsushita@gmail.com>
2021-12-02 18:35:26 -0500

    Skeleton of react app Folders and package.json from previous react apps copied to this repo. A working auth.js from a previous project has been copied to this repo. connection folder and file copied from previously working mongodb project.

---
+++ server/utils/auth.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  // function for our authenticated routes function (req, res, next)
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via  req.query or headers
+    // added req.body.token to copy the class activity example
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      // removed: res.status(400).json({ message: 'You have no token!' })
+      // returning just the req per the class activity example, for this and further examples of error returns
+      return req;
+    }
+
+    // verify token and get user data out of it
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return req;
+    }
+
+    // send to next endpoint
+    next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
